499a24ae69d680d8c77e14bc8cfd2505a07fcb74
kadina21 <93254604+kadina21@users.noreply.github.com>
2024-02-04 02:01:24 -0500

    HomePage CSS

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -7,13 +7,13 @@ function GetStarted() {
       <div className="accroche">
         <h1 className="slogan">Trouvez des trésors cachés</h1>
         <blockquote className="quote">
-          One man's trash is another man's treasure
+          "One man's trash is another man's treasure"
         </blockquote>
         <button className="buttonStart">Get Started</button>
       </div>
       <div className="vendeur">
         <h2 className="invite">
-          Rien à acheter?{" "}
+          Rien à acheter?
           <button className="buttonSell">Je liste mes biens</button>
         </h2>
       </div>
