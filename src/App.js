a92f894fd38f39250e4b670ed697e43790c1b04d
kadina21 <93254604+kadina21@users.noreply.github.com>
2024-02-04 03:07:01 -0500

    update css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Choice from "./pages/Choice";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/choice" element={<Choice />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
---
+++ src/pages/Choice.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "./Choice.css";
+
+function Choice() {
+  return <div className="backgroundImage"></div>;
+}
+
+export default Choice;
